cdfbc4c2fc3c63e5e997d7f6bae11b99baa7bda2
Jerome <jeromerayy@gmail.com>
2017-01-25 13:02:19 -0800

    Program that displays "Hello world!" to the console window.

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+###############################################################################
+#   Programmer:     Jerome Santos
+#   Date:           01/25/2017
+#   Course:         CPT_S 355--Programming Language Design
+#   Term:           Spring 2017
+#   Assignment:     HW1
+#   Description:    A simple program to display "Hello World!"
+###############################################################################
+
+hello = "Hello"
+world = "world"
+
+# Display to the console "Hello world!"
+print(hello + " " + world + "!")
+
+# Requires the user to press enter to exit the console
+input("\nPress enter to exit...")
